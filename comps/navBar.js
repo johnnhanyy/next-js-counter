f00c00622b7267b3f75dcb9ab10fb8f2d315a663
johnnhanyy <85166041+johnnhanyy@users.noreply.github.com>
2022-10-01 17:40:25 +0200

    first commit

---
+++ comps/navBar.js
@@ -0,0 +1,16 @@
+const NavBar = () => {
+  return (
+    <nav style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+       
+      }}>
+      <div>
+        <h1 className="logo">Johns web</h1>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
---
+++ pages/about.js
@@ -0,0 +1,15 @@
+
+const About = () => {
+    return ( 
+        <div>
+            <h1>
+                ABout page 
+            </h1>
+            <p>
+                lorem,
+            </p>
+        </div>
+     );
+}
+ 
+export default About; 
